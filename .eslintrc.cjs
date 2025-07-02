9cbe1bcdc906ce4001d0821e48f6294ed3eb6320
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2025-07-03 00:29:58 +0300

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+};
